b1ef9867373e782ecf771e7be4cad2f12e7443b3
atsushi.shimono <shimono@bug-ja.org>
2013-05-08 19:21:10 +0900

    removed double path name at end

--- skins/table_popup.js
+++ skins/table_popup.js
@@ -50,7 +50,6 @@ var popup_callback = {
           pmes += "/<a href=\"pathinfo.cgi?pid=" + messages.data.parr[ppid].pid + "\">" + messages.data.parr[ppid].name + "</a>";
           ppid = messages.data.parr[ppid].pid;
         }
-        pmes += "/" + messages.data.pathname;
         pmes += "<br>Description: " + messages.data.short_description;
         pmes += "<br>Groups: " + messages.data.gname.join(", ");
       }
